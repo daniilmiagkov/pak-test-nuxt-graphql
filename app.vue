0c8ea307cfa3a3a5da4eca65a5729e8e842607da
daniilmiagkov <59875675+daniilmiagkov@users.noreply.github.com>
2024-03-13 14:18:26 +0300

    init commit

---
+++ app.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+const url = 'http://localhost:8080/graphql'; // Замените на адрес вашего GraphQL-сервера
+
+const query = `
+  query {
+  bookById(id: "book-1") {
+    author {
+      id
+    }
+  }
+}
+`;
+
+
+fetch(url, {
+  method: 'POST',
+  mode: 'no-cors',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ query })
+})
+    .then(response => response.json())
+    .then(data => {
+      console.log('Data received:', JSON.stringify(data));
+      // Обрабатывайте полученные данные здесь
+    })
+    .catch(error => console.error('Error:', error))
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
