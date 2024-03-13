ff3552c8326ec5f2dd332011fec4cd5a39255bb5
daniilmiagkov <59875675+daniilmiagkov@users.noreply.github.com>
2024-03-13 14:52:04 +0300

    сделан успешный запрос к graphql серверу - получен автор по id

--- app.vue
+++ app.vue
@@ -1,37 +1,41 @@
 <template>
-  <div>
-
-  </div>
+  <div> author = {{ author }}</div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "@vue/reactivity";
+import {onMounted} from "@vue/runtime-core";
+
 const url = 'http://localhost:8080/graphql'; // Замените на адрес вашего GraphQL-сервера
+const author = ref({}); // Инициализация реактивной переменной
 
 const query = `
   query {
-  bookById(id: "book-1") {
-    author {
-      id
+    bookById(id: "book-1") {
+      author {
+        id
+      }
     }
   }
-}
 `;
 
+onMounted(() => {
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query })
+  })
+      .then(response => response.json())
+      .then(data => {
+        author.value = data.data.bookById.author.id; // Установка значения author
+        console.log('Data received:', data);
+        // Обрабатывайте полученные данные здесь
+      })
+      .catch(error => console.error('Error:', error))
 
-fetch(url, {
-  method: 'POST',
-  mode: 'no-cors',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({ query })
 })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Data received:', JSON.stringify(data));
-      // Обрабатывайте полученные данные здесь
-    })
-    .catch(error => console.error('Error:', error))
 
 </script>
 
